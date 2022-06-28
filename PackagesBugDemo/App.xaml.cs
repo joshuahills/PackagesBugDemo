2b779fa9c086808b4ad892e19830132760013161
Josh Hills <josh@chimpbyte.co.uk>
2022-06-28 22:44:34 +0100

    Add project files.

---
+++ PackagesBugDemo/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PackagesBugDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private WebApplication? app;
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var logger = LogManager.Setup();
+
+            var builder = WebApplication.CreateBuilder();
+
+            app = builder.Build();
+
+            await app.RunAsync();
+        }
+    }
+}
